2ca23ce6732a50cd3c656f2052b90e8ab89e82a3
Wi1ss0n <150631979+Wi1ss0n@users.noreply.github.com>
2026-03-05 17:30:47 -0800

    Cleaned up the UI (javascript and mobile issues still exist)

--- maps/events/script.js
+++ maps/events/script.js
@@ -1,6 +1,16 @@
 const toggle = document.querySelector(".menu-toggle");
 const sidemenu = document.querySelector(".sidemenu");
 
-toggle.addEventListener("click", () => {
-  sidemenu.classList.toggle("collapsed");
-});
+if (toggle) {
+  toggle.addEventListener("click", () => {
+    sidemenu.classList.toggle("collapsed");
+  });
+}
+
+function handleToggleVisibility() {
+  if (window.innerWidth <= 752 && toggle) {
+    toggle.remove();
+  }
+}
+
+handleToggleVisibility();
