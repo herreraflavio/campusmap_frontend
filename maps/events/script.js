7f9359a80d504aabc0b5378aa363bb6308509db3
Wi1ss0n <150631979+Wi1ss0n@users.noreply.github.com>
2026-03-09 13:23:36 -0700

    Fixed bugs and added sliding animation for side menu

--- maps/events/script.js
+++ maps/events/script.js
@@ -1,21 +1,38 @@
 const toggle = document.querySelector(".menu-toggle");
 const sidemenu = document.querySelector(".sidemenu");
-
 let wasCollapsed = false;
+let resizeTimer;
 
-toggle.addEventListener("click", () => {
+// Desktop
+toggle.addEventListener("click", (e) => {
+  e.stopPropagation();
   sidemenu.classList.toggle("collapsed");
   wasCollapsed = sidemenu.classList.contains("collapsed");
 });
 
+// Mobile
+sidemenu.addEventListener("click", () => {
+  if (window.innerWidth <= 752) {
+    sidemenu.classList.toggle("active");
+  }
+});
+
+// Resize
 function handleResize() {
+  // Disable transitions immediately
+  sidemenu.classList.add("no-transition");
+
   if (window.innerWidth <= 752) {
     sidemenu.classList.remove("collapsed");
   } else {
-    if (wasCollapsed) {
-      sidemenu.classList.add("collapsed"); 
-    }
+    sidemenu.classList.toggle("collapsed", wasCollapsed);
   }
+
+  // Re-enable transitions after a short delay
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    sidemenu.classList.remove("no-transition");
+  }, 150);
 }
 
 window.addEventListener("resize", handleResize);
